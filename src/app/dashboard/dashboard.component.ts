47cb1c3102a9cd217673a73987cfe541c2a2e164
Selmir <selmir543@hotmail.com>
2021-03-10 14:14:12 +0100

    Update Dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+  
+  Designation: string = "";
+  Username: string = "";
+  NoOfTeamMembers: number = 0;
+  TotalCostOfAllProjects: number = 0;
+  PendingTasks: number = 0;
+  UpComingProjects: number = 0;
+  ProjectCost: number = 0;
+  CurrentExpenditure: number = 0;
+  AvailableFunds: number = 0;
 
-  ngOnInit(): void {
+  Clients: string[] = [];
+  Projects: string[] = [];
+  Years: number[] = [];
+  TeamMembersSummary: any = [];
+  TeamMembers: any = [];
+
+  ngOnInit()
+  {
+    this.Designation = 'Team Leader';
+    this.Username = 'Scott Smith';
+    this.NoOfTeamMembers = 67;
+    this.TotalCostOfAllProjects = 240;
+    this.PendingTasks = 15;
+    this.UpComingProjects = 2;
+    this.ProjectCost = 2113507;
+    this.CurrentExpenditure = 96788;
+    this.AvailableFunds = 52536;
+    this.Clients = [
+      'ABC Infotech Ltd.',
+      'DEF Software Solutions',
+      'GHI Industries'
+    ];
+    this.Projects = [
+      'Project A','Project B','Project C', 'Project D'
+    ];
+    for(let i=2019; i>=2010; i--) {
+      this.Years.push(i);
+    }
+
+    this.TeamMembersSummary = [
+      {Region: "East", TeamMembersCount: 20, temporarilyUnvailableMembers: 4 },
+      {Region: "South", TeamMembersCount: 15, temporarilyUnvailableMembers: 8 },
+      {Region: "West", TeamMembersCount: 17, temporarilyUnvailableMembers: 1 },
+      {Region: "North", TeamMembersCount: 15, temporarilyUnvailableMembers: 6 }
+    ];
+
+    this.TeamMembers = [
+      {Region: "East", Members: [
+        {ID: 1, Name: 'Ford', Status: 'Available'},
+        {ID: 2, Name: 'Miller', Status: 'Available'},
+        {ID: 3, Name: 'Jones', Status: 'Busy'},
+        {ID: 4, Name: 'James', Status: 'Busy'},
+      ]},
+      {Region: "South", Members: [
+        {ID: 1, Name: 'Ana', Status: 'Available'},
+        {ID: 2, Name: 'Sebastian', Status: 'Busy'},
+        {ID: 3, Name: 'John', Status: 'Busy'},
+        {ID: 4, Name: 'Terry', Status: 'Busy'},
+      ]},
+      {Region: "West", Members: [
+        {ID: 1, Name: 'Halland', Status: 'Available'},
+        {ID: 2, Name: 'Muhammed', Status: 'Available'},
+        {ID: 3, Name: 'Leo', Status: 'Available'},
+        {ID: 4, Name: 'Jane', Status: 'Available'},
+      ]},
+      {Region: "North", Members: [
+        {ID: 1, Name: 'Lisa', Status: 'Available'},
+        {ID: 2, Name: 'Sarah', Status: 'Available'},
+        {ID: 3, Name: 'Lucas', Status: 'Busy'},
+        {ID: 4, Name: 'Maria', Status: 'Available'},
+      ]},
+    ]
   }
 
 }
